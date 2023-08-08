6f7ad110e55e544a61676887d0751c54ea4a5704
varshi2003 <113238130+varshi2003@users.noreply.github.com>
2023-08-08 16:36:44 +0530

    Create printing_the_repeating_elements_in_an_array.c

---
+++ printing_the_repeating_elements_in_an_array.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {20, 30, 10, 2, 10, 20, 30, 11}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+
+    int visited[n];
+    
+
+    for(int i=0;i<n ;i++)
+    {
+        if(visited[i]==0)
+        {
+            int count=1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                    visited[j]=1;
+                  
+                }
+            }
+            if(count>1)
+            printf("%d \t",arr[i]);
+        }
+    }
+    
+
+    return 0;
+}
